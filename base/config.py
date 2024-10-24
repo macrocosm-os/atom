56aed66351fb3fcbe49a370edfb7f44078c7d683
mccrindlebrian <mccrinbc@gmail.com>
2024-10-24 14:43:49 +0100

    framework for basic miner, validator, and configs

---
+++ base/config.py
@@ -0,0 +1,165 @@
+import os
+import argparse
+import bittensor as bt
+from loguru import logger
+
+def check_config(cls, config: "bt.Config"):
+    r"""Checks/validates the config namespace object."""
+    bt.logging.check_config(config)
+
+    full_path = os.path.expanduser(
+        "{}/{}/{}/netuid{}/{}".format(
+            config.logging.logging_dir,  # TODO: change from ~/.bittensor/miners to ~/.bittensor/neurons
+            config.wallet.name,
+            config.wallet.hotkey,
+            config.netuid,
+            config.neuron.name,
+        )
+    )
+    print("full path:", full_path)
+    config.neuron.full_path = os.path.expanduser(full_path)
+    if not os.path.exists(config.neuron.full_path):
+        os.makedirs(config.neuron.full_path, exist_ok=True)
+
+    if not config.neuron.dont_save_events:
+        # Add custom event logger for the events.
+        if "EVENTS" not in logger._core.levels:
+            logger.level("EVENTS", no=38, icon="📝")
+            logger.add(
+                os.path.join(config.neuron.full_path, "events.log"),
+                rotation=config.neuron.events_retention_size,
+                serialize=False,
+                enqueue=True,
+                backtrace=True,
+                diagnose=False,
+                level="TRACE",
+                format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+            )
+
+
+def add_args(cls, parser):
+    """
+    Adds relevant arguments to the parser for operation.
+    """
+
+    parser.add_argument("--netuid", type=int, help="Subnet netuid", default=1)
+
+    parser.add_argument(
+        "--neuron.device",
+        type=str,
+        help="Device to run on.",
+        default="cpu",
+    )
+
+    parser.add_argument(
+        "--neuron.metagraph_resync_length",
+        type=int,
+        help="The number of blocks until metagraph is resynced.",
+        default=100,
+    )
+
+    parser.add_argument(
+        "--neuron.epoch_length",
+        type=int,
+        help="The default epoch length (how often we set weights, measured in 12 second blocks).",
+        default=500,
+    )
+
+    parser.add_argument(
+        "--neuron.events_retention_size",
+        type=str,
+        help="Events retention size.",
+        default="25 MB",
+    )
+
+    parser.add_argument(
+        "--neuron.dont_save_events",
+        action="store_true",
+        help="If set, we dont save events to a log file.",
+        default=False,
+    )
+
+    parser.add_argument(
+        "--mock",
+        action="store_true",
+        help="Mock neuron and all network components.",
+        default=False,
+    )
+
+
+def add_miner_args(cls, parser):
+    """Add miner specific arguments to the parser."""
+
+    parser.add_argument(
+        "--neuron.name",
+        type=str,
+        help="Trials for this neuron go in neuron.root / (wallet_cold - wallet_hot) / neuron.name. ",
+        default="miner",
+    )
+
+def add_validator_args(cls, parser):
+    """Add validator specific arguments to the parser."""
+
+    parser.add_argument(
+        "--neuron.name",
+        type=str,
+        help="Trials for this neuron go in neuron.root / (wallet_cold - wallet_hot) / neuron.name. ",
+        default="validator",
+    )
+
+    parser.add_argument(
+        "--neuron.timeout",
+        type=float,
+        help="The timeout for each forward call. (seconds)",
+        default=5,
+    )
+
+    parser.add_argument(
+        "--neuron.update_interval",
+        type=float,
+        help="The interval in which the validators query the miners for updates. (seconds)",
+        default=60,
+    )
+
+    parser.add_argument(
+        "--neuron.disable_set_weights",
+        action="store_true",
+        help="Disables setting weights.",
+        default=False,
+    )
+
+    parser.add_argument(
+        "--neuron.moving_average_alpha",
+        type=float,
+        help="Moving average alpha parameter, how much to add of the new observation.",
+        default=0.1,
+    )
+
+    parser.add_argument(
+        "--neuron.axon_off",
+        "--axon_off",
+        action="store_true",
+        # Note: the validator needs to serve an Axon with their IP or they may
+        #   be blacklisted by the firewall of serving peers on the network.
+        help="Set this flag to not attempt to serve an Axon.",
+        default=False,
+    )
+
+    parser.add_argument(
+        "--neuron.vpermit_tao_limit",
+        type=int,
+        help="The maximum number of TAO allowed to query a validator with a vpermit.",
+        default=4096,
+    )
+
+def config(cls):
+    """
+    Returns the configuration object specific to this miner or validator after adding relevant arguments.
+    """
+    parser = argparse.ArgumentParser()
+    bt.wallet.add_args(parser)
+    bt.subtensor.add_args(parser)
+    bt.logging.add_args(parser)
+    bt.axon.add_args(parser)
+    cls.add_args(parser)
+    return bt.config(parser)
---
+++ mock/mock.py
@@ -0,0 +1,66 @@
+import bittensor as bt 
+
+class MockSubtensor(bt.MockSubtensor):
+    def __init__(self, netuid, n=16, wallet=None, network="mock"):
+        super().__init__(network=network)
+
+        if not self.subnet_exists(netuid):
+            self.create_subnet(netuid)
+
+        # Register ourself (the validator) as a neuron at uid=0
+        if wallet is not None:
+            self.force_register_neuron(
+                netuid=netuid,
+                hotkey=wallet.hotkey.ss58_address,
+                coldkey=wallet.coldkey.ss58_address,
+                balance=100000,
+                stake=100000,
+            )
+
+        # Register n mock neurons who will be miners
+        for i in range(1, n + 1):
+            self.force_register_neuron(
+                netuid=netuid,
+                hotkey=f"miner-hotkey-{i}",
+                coldkey="mock-coldkey",
+                balance=100000,
+                stake=100000,
+            )
+
+
+class MockMetagraph(bt.metagraph):
+    def __init__(self, netuid=1, network="mock", subtensor=None):
+        super().__init__(netuid=netuid, network=network, sync=False)
+
+        if subtensor is not None:
+            self.subtensor = subtensor
+        self.sync(subtensor=subtensor)
+
+        for axon in self.axons:
+            axon.ip = "127.0.0.0"
+            axon.port = 8091
+
+        bt.logging.info(f"Metagraph: {self}")
+        bt.logging.info(f"Axons: {self.axons}")
+
+
+class MockDendrite(bt.dendrite):
+    """
+    Replaces a real bittensor network request with a mock request that just returns some static completion for all axons that are passed and adds some random delay.
+    """
+
+    def __init__(self, wallet):
+        super().__init__(wallet)
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the Dendrite object.
+
+        Returns:
+            str: The string representation of the Dendrite object in the format "dendrite(<user_wallet_address>)".
+        """
+        return "MockDendrite({})".format(self.keypair.ss58_address)
+    
+    async def forward(self):
+        #TODO: Implement this function
+        return None
