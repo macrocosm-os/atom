e2a8a22e5b131a3ac484340fa06362dd37bd1d0c
mccrindlebrian <mccrinbc@gmail.com>
2024-12-05 15:05:14 -0500

    comments on tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,7 +1,7 @@
 import pytest
 from atom.mock.mock_identities import MockMiner, MockValidator
 
-
+# Ensure that the miner and validator classes can be instantiated.
 def test_base_miner_neuron():
     miner = MockMiner()
     validator = MockValidator()
--- tests/test_organic.py
+++ tests/test_organic.py
@@ -1,7 +1,7 @@
 from atom.organic_scoring.organic_queue import OrganicQueue
 from atom.mock.mock_identities import MockValidator
 
-
+# Ensure that the organic queue can be instantiated and used.
 def test_organic_queue():
     queue = OrganicQueue()
     assert queue.size == 0
@@ -14,7 +14,7 @@ def test_organic_queue():
 
     assert queue.size == 0
 
-
+# Ensure that the organic validator class can be instantiated.
 def test_organic_validator():
     organic_config = {"trigger_frequency": 1, "trigger": "steps"}
     organic_validator = MockValidator(organic_config=organic_config)
