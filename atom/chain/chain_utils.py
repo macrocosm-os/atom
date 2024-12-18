725df9d15f21e9d0589873f1e2bca5e66e10d222
mccrindlebrian <mccrinbc@gmail.com>
2024-12-17 20:44:50 -0500

    docs for chain_utils

--- atom/chain/chain_utils.py
+++ atom/chain/chain_utils.py
@@ -1,7 +1,20 @@
+"""Various utility functions to be used for chain-related tasks."""
+
+
 import json
+from typing import Union
 
+def json_reader(filepath: str, encoding: str = "utf-8") -> Union[dict, list]:
+    """Reads and parses a JSON file into a Python object.
 
-def json_reader(filepath: str):
-    with open(filepath, "r") as file:
+    Args:
+        filepath (str): Path to the JSON file to be read
+        encoding (str, optional): Character encoding to use when reading the file. Defaults to "utf-8"
+
+    Returns:
+        Union[dict, list]: The parsed JSON content as a Python object - either a dictionary or list 
+                    depending on the JSON structure
+    """
+    with open(filepath, "r", encoding=encoding) as file:
         content = json.load(file)
     return content
